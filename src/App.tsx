b568620ea2a9da6395f8848cda9a71707acdf880
Nathan Victorino <nathanvic.2001@hotmail.com>
2024-03-09 18:04:37 -0300

    images and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carrinho" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
